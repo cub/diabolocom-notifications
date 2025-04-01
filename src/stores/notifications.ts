2ea7fa0b4712eec0749650a5997f58b960ce2bee
cub <hellomynameiscub@gmail.com>
2025-04-01 14:28:52 +0200

    feat: set enum for notifications

--- src/stores/notifications.ts
+++ src/stores/notifications.ts
@@ -2,19 +2,41 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import { v4 as uuidv4 } from 'uuid'
 
+export enum NotificationNamespace {
+  Basic = 'basic',
+  Foo = 'foo',
+  Bar = 'bar',
+}
+
+export enum NotificationType {
+  Success = 'success',
+  Error = 'error',
+  Info = 'info',
+}
+
+export enum NotificationState {
+  Read = 'read',
+  Unread = 'unread',
+}
+
 export type Notification = {
   id: string
+  namespace: NotificationNamespace
   title: string
-  type: 'success' | 'error' | 'info'
+  type: NotificationType
   content: string
-  namespace: string
-  state: 'unread' | 'read'
+  state: NotificationState
 }
 
 export const useNotificationsStore = defineStore('notifications', () => {
   const notifications = ref([] as Notification[])
   function addNotification(notification: Omit<Notification, 'id' | 'state'>) {
-    notifications.value.push({ ...notification, id: uuidv4(), state: 'unread' })
+    notifications.value.push({ ...notification, id: uuidv4(), state: NotificationState.Unread })
   }
-  return { notifications, addNotification }
+  function getNotificationsByNamespace(namespace: NotificationNamespace) {
+    return notifications.value.filter((n) => {
+      return n.namespace === namespace
+    })
+  }
+  return { notifications, addNotification, getNotificationsByNamespace }
 })
