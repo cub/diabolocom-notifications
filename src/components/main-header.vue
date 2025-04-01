55d78efc5d26c12f2a83f26c1127e2092dc8008b
cub <hellomynameiscub@gmail.com>
2025-04-01 14:27:50 +0200

    fix: rm border-x header

--- src/components/main-header.vue
+++ src/components/main-header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header data-name="main-header" class="my-8 border border-neutral-200 bg-white shadow-xl">
+  <header data-name="main-header" class="my-8 border-y border-neutral-200 bg-white shadow-xl">
     <div class="container mx-auto flex items-center gap-8 py-4">
       <img alt="Diabolocom logo" src="@/assets/logo.svg" width="186" height="34" />
       <nav>
