400357ed8431932c6d56c9533cbf2bdefa8aadee
cub <hellomynameiscub@gmail.com>
2025-04-01 22:28:13 +0200

    refactor: svg to file

--- src/components/notifications-list-manage.vue
+++ src/components/notifications-list-manage.vue
@@ -53,64 +53,8 @@
                 "
                 @click="notificationToggleState(n)"
               >
-                <svg
-                  v-if="n.state === NotificationState.Read"
-                  width="24px"
-                  height="24px"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="stroke-neutral-900"
-                >
-                  <path
-                    d="M3 13C6.6 5 17.4 5 21 13"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  ></path>
-                  <path
-                    d="M12 17C10.3431 17 9 15.6569 9 14C9 12.3431 10.3431 11 12 11C13.6569 11 15 12.3431 15 14C15 15.6569 13.6569 17 12 17Z"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  ></path>
-                </svg>
-                <svg
-                  width="24px"
-                  v-if="n.state === NotificationState.Unread"
-                  height="24px"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="stroke-neutral-900"
-                >
-                  <path
-                    d="M19.5 16L17.0248 12.6038"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  ></path>
-                  <path
-                    d="M12 17.5V14"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  ></path>
-                  <path
-                    d="M4.5 16L6.96895 12.6124"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  ></path>
-                  <path
-                    d="M3 8C6.6 16 17.4 16 21 8"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  ></path>
-                </svg>
+                <img src="@/assets/eye.svg" alt="" v-if="n.state === NotificationState.Read" />
+                <img src="@/assets/eye-closed.svg" alt="" v-else />
               </button>
             </div>
             <button
@@ -119,28 +63,7 @@
               aria-label="Delete this notification"
               @click="notificationDelete(n)"
             >
-              <svg
-                width="24px"
-                height="24px"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                class="stroke-neutral-900"
-              >
-                <path
-                  d="M20 9L18.005 20.3463C17.8369 21.3026 17.0062 22 16.0353 22H7.96474C6.99379 22 6.1631 21.3026 5.99496 20.3463L4 9"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></path>
-                <path
-                  d="M21 6L15.375 6M3 6L8.625 6M8.625 6V4C8.625 2.89543 9.52043 2 10.625 2H13.375C14.4796 2 15.375 2.89543 15.375 4V6M8.625 6L15.375 6"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></path>
-              </svg>
+              <img src="@/assets/trash.svg" alt="" />
             </button>
           </div>
         </li>
